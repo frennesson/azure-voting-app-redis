772a2c275922f5704c6205daf1bf154f4fd58313
Magnus Frennesson <magnus.frennesson@gmail.com>
2022-03-07 10:55:02 +0100

    Update test_container.ps1

--- scripts/test_container.ps1
+++ scripts/test_container.ps1
@@ -3,7 +3,7 @@ do{
     $count++
     Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count"
 
-    $testStart = Invoke-Webrequest -Uri http://localhost:80
+    $testStart = Invoke-Webrequest -Uri http://localhost:8080
     
     if ($testStart.statuscode -eq '200') {
         $started = $true
