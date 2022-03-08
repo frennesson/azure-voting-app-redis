048fff2ca429dc203d55d41d1c94a62ab6343976
Magnus Frennesson <magnus.frennesson@humanit.se>
2022-03-08 15:38:28 +0100

    Fixed Typo

--- scripts/test_container.ps1
+++ scripts/test_container.ps1
@@ -1,7 +1,7 @@
 $count = 0
 do{
     $count++
-    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count"
+    Write-Output "[$env:STAGE_NAME] Starting container [Attempt: $count]"
 
     $testStart = Invoke-WebRequest -URI http://localhost:8000
     
